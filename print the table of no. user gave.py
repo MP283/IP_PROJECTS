3b5168c238e2bd1c207ac9b506bf8a0b7f2edaa2
MP283 <moizhakimpatra53@gmail.com>
2022-10-12 21:15:13 +0530

    Create print the table of no. user gave.py

---
+++ print the table of no. user gave.py
@@ -0,0 +1,9 @@
+# print the table of no. user gave
+
+######################################################
+
+n = int(input("Enter a no."))
+i = 1
+while i < 11:
+    print(n, " X ", i, " = ", n*i)
+    i = i + 1
