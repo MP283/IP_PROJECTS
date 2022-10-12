70f6f131aaa6bcb43341658869454eefc356680b
MP283 <moizhakimpatra53@gmail.com>
2022-10-12 21:13:44 +0530

    Create check if no is odd or even.py

---
+++ check if no is odd or even.py
@@ -0,0 +1,10 @@
+# check if no is odd or even
+
+######################################################
+
+print("")
+n = int(input("input no."))
+if n % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
